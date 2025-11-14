1a3052e1cfac1f9f146997ee415d28488537c74a
nazhmimaulanahawari <nazmimln15@gmail.com>
2025-11-14 18:08:43 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+$DB_HOST = '127.0.0.1';
+$DB_USER = 'root';
+$DB_PASS = '';
+$DB_NAME = 'todolist_db';
+
+$conn = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+if (!$conn) {
+    die('Koneksi gagal: ' . mysqli_connect_error());
+}
+mysqli_set_charset($conn, 'utf8mb4');
+?>
